380acdc79cd6736dc6d8aa50497e9f11003e85f2
klapouchy86 <m.jelski86@gmail.com>
2024-02-04 12:11:33 +0100

    Dodaj pliki projektów.

---
+++ SimpleDiceGame/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDiceGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to roll the dice.");
+        }
+    }
+}
