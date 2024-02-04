ed4b7034576558bca2fbded6053c9ebdc0abb6b2
klapouchy86 <m.jelski86@gmail.com>
2024-02-04 12:36:49 +0100

    player vs enemy competition added

--- SimpleDiceGame/Program.cs
+++ SimpleDiceGame/Program.cs
@@ -12,6 +12,10 @@ namespace SimpleDiceGame
         {
             int playerRandomNum;
             int enemyRandomNum;
+
+            int playerPoints = 0;
+            int enemyPoints = 0;
+
             Random random = new Random();
 
             for (int i = 0; i < 10; i++)
@@ -27,7 +31,37 @@ namespace SimpleDiceGame
 
                 enemyRandomNum = random.Next(1, 7);
                 Console.WriteLine("Enemy rolled a " + enemyRandomNum);
+
+                if(playerRandomNum > enemyRandomNum) 
+                {
+                    playerPoints++;
+                    Console.WriteLine("Player wins this round!");
+                }
+                else if (enemyRandomNum > playerRandomNum)
+                {
+                    enemyPoints++;
+                    Console.WriteLine("Enemy wins this round!");
+                }
+                else
+                {
+                    Console.WriteLine("Draw...");
+                }
+                Console.WriteLine("The score is now - Player: " + playerPoints + ". Enemy: " + enemyPoints + ".");
+                Console.WriteLine();
             }
+            if(playerPoints > enemyPoints)
+            {
+                Console.WriteLine("You win! You have" + playerPoints + "points");
+            }
+            else if(enemyPoints > playerPoints) 
+            {
+                Console.WriteLine("Enemy win! He have" + playerPoints + "points");
+            }
+            else
+            {
+                Console.WriteLine("It's a draw!");
+            }
+
             Console.ReadKey();
         }
     }
